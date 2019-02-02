a817840fddd531f7b0cadc9d74de23ea0329c491
Kunihiko Miyanaga <miyanaga@gmail.com>
2019-02-03 08:33:28 +0900

    Add png8 analyize

--- test/test-analyze.js
+++ test/test-analyze.js
@@ -9,7 +9,7 @@ test('Analyze dices.png from path', async t => {
     IDAT: { count: 1, size: 383532 },
     IEND: { count: 1, size: 0 },
     IHDR: { count: 1, size: 13 }
-  });
+  })
 })
 
 test('Analyze dices.png from buffer', async t => {
@@ -19,5 +19,19 @@ test('Analyze dices.png from buffer', async t => {
     IDAT: { count: 1, size: 383532 },
     IEND: { count: 1, size: 0 },
     IHDR: { count: 1, size: 13 }
-  });
+  })
+})
+
+test('Analyze dices8.png from buffer', async t => {
+  const buffer = Fs.readFileSync(Path.join(__dirname, 'data/dices8.png'))
+  const result = await PngStrip.analyze(buffer)
+  t.deepEqual(result, {
+    IHDR: { count: 1, size: 13 },
+    gAMA: { count: 1, size: 4 },
+    sRGB: { count: 1, size: 1 },
+    PLTE: { count: 1, size: 768 },
+    tRNS: { count: 1, size: 247 },
+    IDAT: { count: 8, size: 60299 },
+    IEND: { count: 1, size: 0 }
+  })
 })
