1f1bb3930ebdf225098b8bf895d9b57cae2ff06e
Kunihiko Miyanaga <miyanaga@gmail.com>
2019-02-03 09:58:23 +0900

    Add strip

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const Extract = require('png-chunks-extract'),
   Text = require('png-chunk-text'),
   Fs = require('fs')
 
+const types = {
+  core: ['IHDR', 'PLTE', 'IDAT', 'IEND'],
+  transparency: ['tRNS'],
+  colorSpace: ['gAMA', 'cHRM', 'sRGB', 'iCCP'],
+  textual: ['tEXt', 'zTXt', 'iTXt'],
+  misc: ['bKGD', 'pHYs', 'sBIT', 'sPLT', 'hIST', 'tIME'],
+};
+
 async function toBuffer(png) {
   if (png instanceof Buffer) return png
+
   return new Promise((resolve, reject) => {
     Fs.readFile(png, (err, buffer) => {
       if (err) return reject(err)
@@ -26,6 +35,25 @@ async function analyze(png) {
   return aggs
 }
 
+async function strip(png, keep=[types.core, types.transparency, types.colorSpace]) {
+  const flatten = keep.reduce((flat, t) => {
+    if (t instanceof Array)
+      flat = flat.concat(t)
+    else
+      flat.push(t)
+    return flat
+  }, [])
+
+  const buffer = await toBuffer(png)
+  const chunks = Extract(buffer)
+
+  const strip = chunks.filter(chunk => flatten.includes(chunk.name))
+  return new Buffer(Encode(strip))
+}
+
 module.exports = {
+  types,
+  toBuffer,
   analyze,
+  strip,
 }
---
+++ test/test-strip.js
@@ -0,0 +1,49 @@
+import test from 'ava'
+import PngStrip from '../'
+import Path from 'path'
+import Extract from 'png-chunks-extract'
+import Encode from 'png-chunks-encode'
+
+function utilSortedChunkNameSet(buffer) {
+  const chunks = Extract(buffer)
+  const set = new Set(chunks.map(c => c.name).sort())
+  return set
+}
+
+test.beforeEach(async t => {
+  const dices8 = await PngStrip.toBuffer(Path.join(__dirname, 'data/dices8.png'))
+  const chunks = Extract(dices8)
+
+  // Insert dummy chunks
+  const types = ['cHRM', 'tEXt', 'zTXt', 'iTXt', 'bKGD', 'pHYs', 'sBIT', 'hIST', 'tIME', 'dMMy'];
+  for (let t of types) {
+    const c = { name: t, data: new Uint8Array([]) }
+    chunks.splice(-1, 0, c)
+  }
+
+  t.context.withAllChunks = new Buffer(Encode(chunks))
+})
+
+test('Default Keeps core, transparency and colorSpace', async t => {
+  const buffer = await PngStrip.strip(t.context.withAllChunks)
+  const set = utilSortedChunkNameSet(buffer)
+  t.deepEqual(set, new Set(['IDAT', 'IEND', 'IHDR', 'PLTE', 'cHRM', 'gAMA', 'sRGB', 'tRNS']))
+})
+
+test('Keep only core', async t => {
+  const buffer = await PngStrip.strip(t.context.withAllChunks, PngStrip.types.core)
+  const set = utilSortedChunkNameSet(buffer)
+  t.deepEqual(set, new Set(['IDAT', 'IEND', 'IHDR', 'PLTE']))
+})
+
+test('Keep only core and misc', async t => {
+  const buffer = await PngStrip.strip(t.context.withAllChunks, [PngStrip.types.core, PngStrip.types.misc])
+  const set = utilSortedChunkNameSet(buffer)
+  t.deepEqual(set, new Set(['IDAT', 'IEND', 'IHDR', 'PLTE', 'bKGD', 'hIST', 'pHYs', 'sBIT', 'tIME']))
+})
+
+test('Keep only core and dummy', async t => {
+  const buffer = await PngStrip.strip(t.context.withAllChunks, [PngStrip.types.core, 'dMMy'])
+  const set = utilSortedChunkNameSet(buffer)
+  t.deepEqual(set, new Set(['IDAT', 'IEND', 'IHDR', 'PLTE', 'dMMy']))
+})
